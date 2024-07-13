575659423a280868798308b386986a738d6d054f
Prajwal-KS <135096407+Prajwal-KS@users.noreply.github.com>
2024-07-13 17:11:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def recommend(movie):
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
-    for i in distances[1:6]:
+    for i in distances[1:21]:
         
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
@@ -58,6 +58,9 @@ selected_movie = st.selectbox(
 if st.button('Show Recommendation'):
     recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
+    col6, col7, col8, col9, col10 = st.columns(5)
+    col11, col12, col13, col14, col15 = st.columns(5)
+    col16, col17, col18, col19, col20 = st.columns(5)
     with col1:
         st.write(recommended_movie_names[0])
         st.image(recommended_movie_posters[0])
@@ -73,4 +76,49 @@ if st.button('Show Recommendation'):
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
+    with col6:
+        st.write(recommended_movie_names[5])
+        st.image(recommended_movie_posters[5])
+    with col7:
+        st.text(recommended_movie_names[6])
+        st.image(recommended_movie_posters[6])
+    with col8:
+        st.text(recommended_movie_names[7])
+        st.image(recommended_movie_posters[7])
+    with col9:
+        st.text(recommended_movie_names[8])
+        st.image(recommended_movie_posters[8])
+    with col10:
+        st.text(recommended_movie_names[9])
+        st.image(recommended_movie_posters[9])
+    with col11:
+        st.write(recommended_movie_names[10])
+        st.image(recommended_movie_posters[10])
+    with col12:
+        st.text(recommended_movie_names[11])
+        st.image(recommended_movie_posters[11])
+    with col13:
+        st.text(recommended_movie_names[12])
+        st.image(recommended_movie_posters[12])
+    with col14:
+        st.text(recommended_movie_names[13])
+        st.image(recommended_movie_posters[13])
+    with col15:
+        st.text(recommended_movie_names[14])
+        st.image(recommended_movie_posters[14])
+    with col16:
+        st.write(recommended_movie_names[15])
+        st.image(recommended_movie_posters[15])
+    with col17:
+        st.text(recommended_movie_names[16])
+        st.image(recommended_movie_posters[16])
+    with col18:
+        st.text(recommended_movie_names[17])
+        st.image(recommended_movie_posters[17])
+    with col19:
+        st.text(recommended_movie_names[18])
+        st.image(recommended_movie_posters[18])
+    with col20:
+        st.text(recommended_movie_names[19])
+        st.image(recommended_movie_posters[19])
 
